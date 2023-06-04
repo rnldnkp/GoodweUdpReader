f2f2573350942a1b4744780cd4aa339276bb983d
rnldnkp <25203654+rnldnkp@users.noreply.github.com>
2023-06-04 23:11:09 +0200

    3 phase support
    Added multi-string and 3-phase support. Details do not report to PvOutput just yet.

--- GoodWeOutput.php
+++ GoodWeOutput.php
@@ -9,22 +9,32 @@ final class GoodWeOutput
     const WORK_MODE_ERROR = 2;
     const WORK_MODE_CHECK = 4;
 
-    protected $currentDc1;
-    protected $voltDc1;
-    protected $voltAc1;
-    protected $currentAc1;
-    protected $frequencyAc1;
-    protected $rssi;
-    protected $totalHours;
-    protected $generationTotal;
-    protected $generationToday;
-    protected $temperature;
-    protected $workMode;
-    protected $power;
     /**
      * @var \DateTime
      */
     private $dateTime;
+
+    protected $voltDc1;
+    protected $currentDc1;
+    protected $voltDc2;
+    protected $currentDc2;
+    protected $voltAc1;
+    protected $voltAc2;
+    protected $voltAc3;
+    protected $currentAc1;
+    protected $currentAc2;
+    protected $currentAc3;
+    protected $frequencyAc1;
+    protected $frequencyAc2;
+    protected $frequencyAc3;
+    protected $power;
+    protected $workMode;
+    protected $temperature;
+    protected $generationToday;
+    protected $generationTotal;
+    protected $totalHours;
+    protected $setSafetyCode;
+    protected $rssi;
 
     public function __construct()
     {
@@ -48,9 +58,17 @@ final class GoodWeOutput
             'dateTime' => $this->dateTime->format(DATE_ATOM),
             'voltDc1' => $this->voltDc1,
             'currentDc1' => $this->currentDc1,
+            'voltDc2' => $this->voltDc2,
+            'currentDc2' => $this->currentDc2,
             'voltAc1' => $this->voltAc1,
+            'voltAc2' => $this->voltAc2,
+            'voltAc3' => $this->voltAc3,
             'currentAc1' => $this->currentAc1,
+            'currentAc2' => $this->currentAc2,
+            'currentAc3' => $this->currentAc3,
             'frequencyAc1' => $this->frequencyAc1,
+            'frequencyAc2' => $this->frequencyAc2,
+            'frequencyAc3' => $this->frequencyAc3,
             'power' => $this->power,
             'workMode' => $this->workMode,
             'readableWorkMode' => $this->getReadableWorkMode(),
@@ -58,23 +76,34 @@ final class GoodWeOutput
             'generationToday' => $this->generationToday,
             'generationTotal' => $this->generationTotal,
             'totalHours' => $this->totalHours,
+            'SafetyCode' => $this->SafetyCode,
             'rssi' => $this->rssi,
         ];
     }
 
-    public function setVoltDc1($volt)
+    public function setVoltDc1($voltDc1)
     {
-        $this->voltDc1 = $volt;
+        $this->voltDc1 = $voltDc1;
     }
 
-    public function setCurrentDc1($current)
+    public function setCurrentDc1($currentDc1)
     {
-        $this->currentDc1 = $current;
+        $this->currentDc1 = $currentDc1;
     }
 
-    public function setVoltAc1($voltage)
+    public function setVoltDc2($voltDc2)
     {
-        $this->voltAc1 = $voltage;
+        $this->voltDc2 = $voltDc2;
+    }
+
+    public function setCurrentDc2($currentDc2)
+    {
+        $this->currentDc2 = $currentDc2;
+    }
+
+    public function setVoltAc1($voltageAc1)
+    {
+        $this->voltAc1 = $voltageAc1;
     }
 
     public function getVoltageAc1()
@@ -82,14 +111,52 @@ final class GoodWeOutput
         return $this->voltAc1;
     }
 
-    public function setCurrentAc1($current)
+    public function setVoltAc2($voltAc2)
     {
-        $this->currentAc1 = $current;
+        $this->voltAc2 = $voltAc2;
+    }
+    public function getVoltageAc2()
+    {
+        return $this->voltAc2;
     }
 
-    public function setFrequencyAc1($frequency)
+    public function setVoltAc3($voltAc3)
     {
-        $this->frequencyAc1 = $frequency;
+        $this->voltAc3 = $voltAc3;
+    }
+    public function getVoltageAc3()
+    {
+        return $this->voltAc3;
+    }
+
+    public function setCurrentAc1($currentAc1)
+    {
+        $this->currentAc1 = $currentAc1;
+    }
+
+    public function setCurrentAc2($currentAc2)
+    {
+        $this->currentAc2 = $currentAc2;
+    }
+
+    public function setCurrentAc3($currentAc3)
+    {
+        $this->currentAc3 = $currentAc3;
+    }
+
+    public function setFrequencyAc1($frequencyAc1)
+    {
+        $this->frequencyAc1 = $frequencyAc1;
+    }
+
+    public function setFrequencyAc2($frequencyAc2)
+    {
+        $this->frequencyAc2 = $frequencyAc2;
+    }
+
+    public function setFrequencyAc3($frequencyAc3)
+    {
+        $this->frequencyAc3 = $frequencyAc3;
     }
 
     public function setPower($power)
@@ -137,6 +204,11 @@ final class GoodWeOutput
         $this->totalHours = $hours;
     }
 
+    public function setSafetyCode($SafetyCode)
+    {
+        $this->SafetyCode = $SafetyCode;
+    }
+
     public function setRSSI($rssi)
     {
         $this->rssi = $rssi;
@@ -160,9 +232,17 @@ final class GoodWeOutput
 
         echo 'DC1 Voltage   ' . $this->voltDc1 . 'V' . PHP_EOL;
         echo 'DC1 Current   ' . $this->currentDc1 . 'A' . PHP_EOL;
+        echo 'DC2 Voltage   ' . $this->voltDc2 . 'V' . PHP_EOL;
+        echo 'DC2 Current   ' . $this->currentDc2 . 'A' . PHP_EOL;
         echo 'AC1 Voltage   ' . $this->voltAc1 . 'V' . PHP_EOL;
+        echo 'AC2 Voltage   ' . $this->voltAc2 . 'V' . PHP_EOL;
+        echo 'AC3 Voltage   ' . $this->voltAc3 . 'V' . PHP_EOL;
         echo 'AC1 Current   ' . $this->currentAc1 . 'A' . PHP_EOL;
+        echo 'AC2 Current   ' . $this->currentAc2 . 'A' . PHP_EOL;
+        echo 'AC3 Current   ' . $this->currentAc3 . 'A' . PHP_EOL;
         echo 'AC1 Frequency ' . $this->frequencyAc1 . 'Hz' . PHP_EOL;
+        echo 'AC2 Frequency ' . $this->frequencyAc2 . 'Hz' . PHP_EOL;
+        echo 'AC3 Frequency ' . $this->frequencyAc3 . 'Hz' . PHP_EOL;
         echo 'Power         ' . $this->power . 'kW'. PHP_EOL;
         echo 'WorkMode      ' . $this->workMode . PHP_EOL;
         echo 'WorkMode      ' . $this->getReadableWorkMode() . PHP_EOL;
@@ -170,6 +250,7 @@ final class GoodWeOutput
         echo 'Energy Today  ' . $this->generationToday . 'kWh' . PHP_EOL;
         echo 'Energy Total  ' . $this->generationTotal . 'kWh' . PHP_EOL;
         echo 'Total hours   ' . $this->totalHours . 'h' . PHP_EOL;
-        echo 'WiFi RSSI:    ' . $this->rssi . '%' . PHP_EOL;
+        echo 'SafetyCode    ' . $this->SafetyCode . '-' . PHP_EOL;
+        echo 'WiFi RSSI     ' . $this->rssi . '%' . PHP_EOL;
     }
 }
--- GoodWeProcessor.php
+++ GoodWeProcessor.php
@@ -15,15 +15,24 @@ final class GoodWeProcessor
         $goodweOutput->setDateTime($date);
         $goodweOutput->setVoltDc1(hexdec(bin2hex($binary[11] . $binary[12])) / 10);
         $goodweOutput->setCurrentDc1(hexdec(bin2hex($binary[13]. $binary[14])) / 10);
+        $goodweOutput->setVoltDc2(hexdec(bin2hex($binary[15] . $binary[16])) / 10);
+        $goodweOutput->setCurrentDc2(hexdec(bin2hex($binary[17]. $binary[18])) / 10);
         $goodweOutput->setVoltAc1(hexdec(bin2hex($binary[41] . $binary[42])) / 10);
+        $goodweOutput->setVoltAc2(hexdec(bin2hex($binary[43] . $binary[44])) / 10);
+        $goodweOutput->setVoltAc3(hexdec(bin2hex($binary[45] . $binary[46])) / 10);
         $goodweOutput->setCurrentAc1(hexdec(bin2hex($binary[47] . $binary[48])) / 10);
+        $goodweOutput->setCurrentAc2(hexdec(bin2hex($binary[49] . $binary[50])) / 10);
+        $goodweOutput->setCurrentAc3(hexdec(bin2hex($binary[51] . $binary[52])) / 10);
         $goodweOutput->setFrequencyAc1(hexdec(bin2hex($binary[53] . $binary[54])) / 100);
+        $goodweOutput->setFrequencyAc2(hexdec(bin2hex($binary[55] . $binary[56])) / 100);
+        $goodweOutput->setFrequencyAc3(hexdec(bin2hex($binary[57] . $binary[58])) / 100);
         $goodweOutput->setPower(hexdec(bin2hex($binary[61] . $binary[62])) / 1000);
         $goodweOutput->setWorkMode(hexdec(bin2hex($binary[63] . $binary[64])));
         $goodweOutput->setTemperature(hexdec(bin2hex($binary[87] . $binary[88])) / 10);
         $goodweOutput->setGenerationToday(hexdec(bin2hex($binary[93] . $binary[94])) / 10);
         $goodweOutput->setGenerationTotal(hexdec(bin2hex($binary[95] . $binary[96] . $binary[97] . $binary[98])) / 10);
         $goodweOutput->setTotalHours(hexdec(bin2hex($binary[99] . $binary[100] . $binary[101] . $binary[102])));
+        $goodweOutput->setSafetyCode(hexdec(bin2hex($binary[103] . $binary[104])) / 100);
         $goodweOutput->setRSSI(hexdec(bin2hex($binary[149] . $binary[150])));
 
         return $goodweOutput;
